97a383f45bb946ca75892934e005457702591881
Jackton <team@jackton.xyz>
2026-04-12 03:06:37 +0700

    fix: allow remote images from ImgBB and Unsplash

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,17 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.jackton.xyz',
+  image: {
+    domains: ['i.ibb.co', 'images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      }
+    ],
+  },
 });
